2346defa23aad0710718b71247640ef954ff6079
Mostyn Griffith (NewMAC) <mostyngriffith@mostyns-mbp-3.risd.edu>
2017-12-04 21:09:17 -0500

    final show_ 12/4

--- scripts.js
+++ scripts.js
@@ -1,19 +1,75 @@
 $(document).ready(function(){
 
-let lazy = lazyload();
-    $("img.lazyload").lazyload();
-});
+	$(".coinhive-cover").on("click", function(){
+    $(this).hide();
+    $("#after").show();
+    $("#burst").show();
+	  $(".intro").hide();
+	});
 
-// var $cog = $('.cog'),
-//     $body = $(document.body),
-//     bodyHeight = $body.height();
 
-// $(window).scroll(function () {
-//     $cog.css({
-//         'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 360) + 'deg)'
-//     });
+
+  $(".voter").on("click", function(){
+    $(".voteCover").show();
+  });
 
 $(window).scroll(function() {
-var theta = $(window).scrollTop() / 100 % Math.PI;
+var theta = $(window).scrollTop() / 500 % Math.PI;
 $('#cog').css({ transform: 'rotate(' + theta + 'rad)' });
 });
+});
+
+var $img = $('#burst');
+// setInterval(function(){
+//     $('#after').toggle(function(){
+//         $img.show();
+//         setTimeout(function() {
+//             $img.attr('src', 'http://i.imgur.com/JfkmXjG.gif');
+//         }, 0);
+//     }, function(){
+//         $img.hide();
+//     });
+// }, 10000);
+
+setInterval(function(){
+  $("#coin_gif").toggle();
+  setTimeout(function() {
+      $("#coin_gif").attr('src', 'images/coin_burst_lg.gif');
+  }, 0);
+}, 5000);
+
+// setInterval(function(){
+//   $("#coin_gif").hide();
+// }, 10000);
+
+
+
+jQuery(window).scroll(function(){
+    var fromTopPx = 900; // distance to trigger
+    var scrolledFromtop = jQuery(window).scrollTop();
+    if(scrolledFromtop > fromTopPx){
+        jQuery('body').addClass('members');
+      }else{
+        jQuery('body').removeClass('members');
+      }
+
+
+
+    // }else{
+    //     jQuery('body').removeClass('members');
+    // }
+});
+//
+// var document.getElementById('after')
+// var text = ["Welcome", "Hi", "Sup dude"];
+// var counter = 0;
+// var elem = document.getElementById("changeText");
+// setInterval(change, 1000);
+//
+// function change() {
+//   elem.innerHTML = text[counter];
+//   counter++;
+//   if (counter >= text.length) {
+//     counter = 0;
+//   }
+// }
